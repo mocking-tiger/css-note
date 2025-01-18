76bde64d46162b409b76ba717201a6db23284065
Park JunYong <dyden119@gmail.com>
2025-01-18 23:51:00 +0900

    chore: 프로젝트 초기화

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>홈홈홈홈홈홈홈홈홈홈홈홈홈홈홈홈홈홈홈홈홈홈</div>;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+  *{
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Pretendard-Regular',sans-serif;
+  }
+`;
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>
+);
